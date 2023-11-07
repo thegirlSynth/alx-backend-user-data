245e6c16188e333d650dbd27d556f76b36728433
Cynthia Uche - [Linux] <cynthianuchepro@gmail.com>
2023-11-07 17:48:48 +0100

    Properly document the auth module

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """
+This module contains the auth class
 """
 
 from flask import request
@@ -12,13 +13,19 @@ class Auth:
     """
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """ """
+        """
+        Requires authentication
+        """
         return False
 
     def authorization_header(self, request=None) -> str:
-        """ """
+        """
+        Authorization header
+        """
         return None
 
     def current_user(self, request=None) -> TypeVar("User"):
-        """"""
+        """
+        Current User
+        """
         return None
