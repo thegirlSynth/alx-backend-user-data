e0e575ad0097cbbe1666081818c9f5304301024c
Cynthia Uche - [Linux] <cynthianuchepro@gmail.com>
2023-11-08 05:57:18 +0100

    Extract Base64 part

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -10,4 +10,15 @@ class BasicAuth(Auth):
     This is a class for basic authentication
     """
 
-    pass
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """
+        Returns the Base64 part of the Authorization header
+        for a Basic Authentication
+        """
+        if (
+            not authorization_header
+            or not isinstance(authorization_header, str)
+            or not authorization_header.startswith("Basic ")
+        ):
+            return None
+        return authorization_header[6:]
