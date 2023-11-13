b1b1db194f08a1e9d203334fc88eef7bfcc741ef
Cynthia Uche - [Linux] <cynthianuchepro@gmail.com>
2023-11-13 16:42:24 +0100

    Correct function annotation

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -10,7 +10,9 @@ import re
 logging.basicConfig()
 
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str):
+def filter_datum(
+    fields: List[str], redaction: str, message: str, separator: str
+) -> str:
     """Returns the log message obfuscated"""
     for field in fields:
         message = re.sub(
