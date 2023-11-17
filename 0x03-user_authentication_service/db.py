697807a64407d6fa71ed2394774404ff764dc6ae
Cynthia Uche - [Linux] <cynthianuchepro@gmail.com>
2023-11-17 02:45:37 +0100

    Type-annotate add_user  function

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -33,7 +33,7 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email: str, hashed_password: str):
+    def add_user(self, email: str, hashed_password: str) -> User:
         """
         Saves a new user to the database
         """
