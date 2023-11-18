3708ffe1266fc6759e48ba92b662e2067ac04812
Cynthia Uche - [Linux] <cynthianuchepro@gmail.com>
2023-11-18 01:25:27 +0100

    User Profile

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -80,10 +80,12 @@ def profile():
 
     session_id = request.cookies.get("session_id")
     user = AUTH.get_user_from_session_id(session_id)
+
     if not user:
         abort(403)
 
-    return jsonify({"email": "<user email>"}), 200
+    email = getattr(user, "email")
+    return jsonify({"email": email}), 200
 
 
 if __name__ == "__main__":
