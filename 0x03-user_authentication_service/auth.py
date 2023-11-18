29c643ad896dfba4a12162c99a28afefb2d1af16
Cynthia Uche - [Linux] <cynthianuchepro@gmail.com>
2023-11-18 17:15:25 +0100

    Update password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -119,3 +119,16 @@ class Auth:
 
         except NoResultFound:
             raise ValueError
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """
+        Update Password
+        """
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+            hashed_password = _hash_password(password)
+            setattr(user, "hashed_password", hashed_password)
+            setattr(user, "reset_token", None)
+
+        except NoResultFound:
+            raise ValueError
